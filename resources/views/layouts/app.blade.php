488753380899164a9119ddd6dfe0c473ba7a8c27
hajricod <48984962+hajricod@users.noreply.github.com>
2024-03-14 00:45:00 +0400

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -118,7 +118,7 @@
                                 {{-- <li><a href="#portfolio">Portfolio</a></li> --}}
                                 <li><a href="#about">About</a></li>
                                 <li><a href="#contact">Contact</a></li>
-                                <li><a href="#" class="external">External</a></li>
+                                {{-- <li><a href="#" class="external">External</a></li> --}}
                             </ul>
                         </div> <!-- /.menu -->
                     </div> <!-- /.responsive-menu -->
